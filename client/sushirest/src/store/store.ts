7f7ced1ee1c1da84efc7a35273cd3b4b1ff99068
Dimativ <88792228+Dimativ@users.noreply.github.com>
2024-01-02 01:00:36 +0200

    Delete client/sushirest directory

--- client/sushirest/src/store/store.ts
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import filter from "./slices/filter/filterSlice.ts";
-import topPosition from "./slices/topPosition/topPositionSlice.ts";
-export const store = configureStore({
-    reducer: {
-        filter,
-        topPosition,
-    },
-})
